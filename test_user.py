05608f9b6a680f15a91fbac28c05a06a0aa30c5e
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-23 14:41:51 +0300

    ft:usertest - runs tests to check if user object is properly initiated

---
+++ test_user.py
@@ -0,0 +1,19 @@
+import unittest # importing the unittest module
+from User import User #importing the user class to our test script
+
+class TestUser(unittest.TestCase):
+    """
+    subclass with unitest.testcase to help us in creation of testcases.
+    the subclass TestUser inherits from the unittest.TestCase
+    """
+    #run a test to see if an object is instantiated correctly
+    def setUp(self):
+        self.new_user = ("Reuby","Doe","DoeReuby","0000")
+    def test_init(self):
+        self.assertEqual(self.new_user.firstname,"Reuby")
+        self.assertEqual(self.new_user.lastname,"Doe")
+        self.assertEqual(self.new_user.username,"DoeReuby") 
+        self.assertEqual(self.new_user.password,"0000")
+if __name__ == '__main__':
+    unittest.main()
+        
