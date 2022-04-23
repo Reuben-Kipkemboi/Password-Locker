359c6873ad1ce15d6132d661a7c5368d87d0c2e0
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-23 15:10:01 +0300

    ft:usertest - saves user to the list

--- test_user.py
+++ test_user.py
@@ -8,12 +8,18 @@ class TestUser(unittest.TestCase):
     """
     #run a test to see if an object is instantiated correctly
     def setUp(self):
-        self.new_user = User("Reuby","Doe","DoeReuby","0000")
+        #creating our new user object
+        self.new_user = User("Reuby","Doe","DoeReuby","0000") 
     def test_init(self):
         self.assertEqual(self.new_user.firstname,"Reuby")
         self.assertEqual(self.new_user.lastname,"Doe")
         self.assertEqual(self.new_user.username,"DoeReuby") 
         self.assertEqual(self.new_user.password,"0000")
+        
+    #saing our new user 
+    def test_save_User(self):
+        self.new_user.save_User()
+        self.assertEqual(len(User.user_list),1)
 if __name__ == '__main__':
     unittest.main()
         
