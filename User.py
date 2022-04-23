14c2f1ea3267c4c4c5206721c96680932ede528e
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-23 14:15:44 +0300

    ft:userclass - creates a class to hold user properties

---
+++ User.py
@@ -0,0 +1,14 @@
+class User:
+    """
+    initializes a new User class
+    """
+    user_list[] #our list where we shall store our users
+    def __init__(self, firstname, lastname, username, passcode):
+        """
+        the method here for defining properties of the object
+        """
+        self.firstname = firstname
+        self.lastname = lastname
+        self.username = username
+        self.password = passcode
+        
