2ac41cb9efd434319ca5befeeafef55677d8b7d6
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-23 15:27:16 +0300

    ft:usertest - deletes user in the list

--- test_user.py
+++ test_user.py
@@ -10,16 +10,33 @@ class TestUser(unittest.TestCase):
     def setUp(self):
         #creating our new user object
         self.new_user = User("Reuby","Doe","DoeReuby","0000") 
+    def tearDown(self):
+            '''
+            tearDown runs after every test case is complete.
+            '''
+            User.user_list = []
+
     def test_init(self):
         self.assertEqual(self.new_user.firstname,"Reuby")
         self.assertEqual(self.new_user.lastname,"Doe")
         self.assertEqual(self.new_user.username,"DoeReuby") 
         self.assertEqual(self.new_user.password,"0000")
         
-    #saing our new user 
+    #saving our new user 
     def test_save_User(self):
         self.new_user.save_User()
         self.assertEqual(len(User.user_list),1)
+        
+    #Deleting the user and his details
+    def test_delete_User(self):
+        #To delete we save another user then later check if the list still has the other user
+        self.new_user.save_User()
+        another_user = User("John", "Doe", "JohnDoe", "1111")
+        another_user.save_User()
+        #Lets perform the delete function and assert to see if the list length will reduce
+        self.new_user.delete_User()
+        self.assertEqual(len(User.user_list),1)
+        
 if __name__ == '__main__':
     unittest.main()
         
