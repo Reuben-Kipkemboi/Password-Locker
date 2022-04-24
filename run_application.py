66f044d22cab5bbc3502606a8e69463b51d3ca1f
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-04-24 17:49:33 +0300

    ts:run_application.py - tests application functionality

--- run_application.py
+++ run_application.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.8 
 #on top is shebang
-
+import random
+import string #to be used in password generation
 from click import option
 from User import User
 from Credentials import Credentials
@@ -15,11 +16,11 @@ def save_User(User):
 def delete_user(User):
     User.delete_user()
     
-def search_user(passcode):
-    return User.find_by_passcode()
+def find_by_passcode(passcode):
+    return User.find_by_passcode(passcode)
 
 def check_existing_user(passcode):
-    return User.user_exist()
+    return User.user_exist(passcode)
 
 def display_users():
     return User.display_users()
@@ -58,7 +59,7 @@ def main():
             user_input =input()
             if user_input =="CA":
                 print("Proceed to create your password Locker Account")
-                print("--"*40)
+                print("--"*20)
                 print("Enter your First Name")
                 firstname = input()
                 print("Enter your Last Name")
@@ -70,14 +71,49 @@ def main():
                 #Saving the user created credentials
                 save_User(create_user(firstname, lastname, username, passcode))
                 print("Your password-locker account created successfully")
-                print("--" *40)
-                print(f"FullNames -> {firstname} {lastname} \nUsername -> {username} \nPasscode/password -> {passcode}")
-                
-            
-        
-    
-    
-    
+                print("--" *20)
+                print(f"FullNames -> {firstname}{lastname} \n User Name -> {username} \nPasscode/password -> {passcode}")
+            elif user_input == "IN":
+                print("Please provide your user Name")
+                username = input()
+                print("Provide your passcode or Password")
+                passcode = input()
+                if check_existing_user(passcode):
+                    print("\n")
+                    print("Successfully logged in ...Please go ahead and create(C)and view (V)your different account credentials")
+                    print("*-"*40)
+                    print('C or V')
+                    user_selection = input()
+                    print('\n')
+                    if user_selection == "C":
+                        print("Create your various accounts e.g socila media")
+                        print("*-"*40)
+                        print("Provide Account name")
+                        accountname = input()
+                        print("*-"*40)
+                        accountusername = username
+                        print("*-"*40)
+                        print("Provide your account security passcode(CP) or Use Generated Password(GP)")
+                        generatedpassword = input()
+                        if generatedpassword == "GP":
+                            passwordcharacters = string.ascii_letters +string.digits
+                            accountpasscode = "".join(user_selection(passwordcharacters )for i in range(6,16))
+                            print(f"Passcode -> {accountpasscode}")
+                        elif generatedpassword == "CP":
+                            print("Provide your security key or passcode")
+                            accountpasscode = input()
+                        else:
+                            print("provide a better selection")
+                        save_Credentials(create_account(accountname, accountusername, accountpasscode))
+                        print("\n")
+                        print(f"Your account name -> {accountname} \n Account User Name ->  {accountusername} \n Account Passcode ->  {accountpasscode} ")
+                        
+                        
+                            
+                        
+                        
+                                          
+                    
 if __name__ == "__main__":
     main()
 
